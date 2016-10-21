c4a6f741688d98d2f4f614aa4d535bb0793a9174
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-10-22 05:50:31 +0900

    commit

--- src/test/ts/jaconv/all_test.ts
+++ src/test/ts/jaconv/all_test.ts
@@ -10,8 +10,21 @@ namespace x_jaconv {
   }();
 }
 
-exports.jaconv = function(test : any) {
+exports.toHan = function(test : any) {
   test.equal(jaconv.toHan("あアｱＡA１1"), 'あｱｱAA11');
+  test.done();
+};
+
+exports.toZen = function(test : any) {
   test.equal(jaconv.toZen("あアｱＡA１1"), 'あアアＡＡ１１');
   test.done();
 };
+
+exports.toHebon = function(test : any) {
+  test.equal(jaconv.toHebon("にほんばし"), 'NIHOMBASHI');
+  test.equal(jaconv.toHebon("いいだばし"), 'IIDABASHI');
+  test.equal(jaconv.toHebon("おおいた"), 'OITA');
+  test.equal(jaconv.toHebon("とうきょう"), 'TOKYO');
+  test.done();
+};
+
